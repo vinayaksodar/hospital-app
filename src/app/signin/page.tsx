cc74a806a51b6f79df1b6be562e0aa214f174c22
vinayaksodar <vinayakmsodar@gmail.com>
2025-11-22 16:59:09 +0000

    wrap in suspense

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,17 +1,25 @@
-"use client"
+"use client";
 
-import { LoginForm } from "@/components/login-form"
+import { LoginForm } from "@/components/login-form";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-export default function Page() {
+function SignInFormWrapper() {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get("callbackUrl") || "/";
+  return <LoginForm callbackUrl={callbackUrl} />;
+}
+
+export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <LoginForm callbackUrl={callbackUrl} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <SignInFormWrapper />
+        </Suspense>
       </div>
     </div>
-  )
+  );
 }
+
 
