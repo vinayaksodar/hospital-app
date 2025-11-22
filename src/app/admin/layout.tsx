886eef9e5da0eb1199a7bad717db251f773f54a3
vinayaksodar <vinayakmsodar@gmail.com>
2025-11-22 16:15:14 +0000

    remove auth from middleware

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,11 +1,17 @@
+import { auth } from "@/lib/auth/auth";
 import { AdminSidebar } from "@/components/admin-sidebar";
 import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+import { redirect } from "next/navigation";
 
-export default function AdminLayout({
+export default async function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth();
+  if (!session?.user || session.user.role !== "admin") {
+    return redirect("/signin");
+  }
   return (
     <SidebarProvider>
       <AdminSidebar />
